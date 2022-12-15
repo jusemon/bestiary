3eec4164ad1d3d9b50433139ca9c307564fc0c72
Jusemon <juansmm@outlook.com>
2022-12-14 23:47:33 -0500

    Added location search support

--- src/components/search-bar/search-bar.tsx
+++ src/components/search-bar/search-bar.tsx
@@ -1,5 +1,5 @@
 import React, { ChangeEvent } from 'react';
-import { SearchBarProps, SearchType, SearchBarActions } from './types';
+import { SearchBarProps, SearchType, SearchBarActions, SearchBarState } from './types';
 import './search-bar.scss';
 import { AppState } from '../app/types';
 import { Dispatch } from 'redux';
@@ -7,39 +7,43 @@ import { searchEnemies } from './actions';
 import { Enemy } from '../result-item/types';
 import { connect } from 'react-redux';
 
-class SearchBar extends React.Component<SearchBarProps, { value: string, searchType: string }> {
+class SearchBar extends React.Component<SearchBarProps, SearchBarState> {
 
   constructor(props: SearchBarProps) {
     super(props);
-    this.state = { value: '', searchType: SearchType.Item.toString() };
-    this.handleSearchChange = this.handleSearchChange.bind(this);
-    this.handleSearchTypeChange = this.handleSearchTypeChange.bind(this);
+    this.state = { search: '', searchType: SearchType.Item };
+    this.props.search(this.state.search, this.state.searchType, this.props.enemies);
+    console.log('constructor');
   }
 
   handleSearchChange(event: ChangeEvent<HTMLInputElement>) {
-    this.setState({ value: event.target.value });
-    this.props.search(this.state.value, this.state.searchType as SearchType, this.props.enemies)
+    this.setState({ search: event.target.value });
   }
 
   handleSearchTypeChange(event: ChangeEvent<HTMLSelectElement>) {
-    this.setState({ searchType: event.target.value });
-    this.props.search(this.state.value, this.state.searchType as SearchType, this.props.enemies)
+    this.setState({ searchType: event.target.value as SearchType });
+  }
+
+  componentDidUpdate(_: SearchBarProps, state: SearchBarState) {
+    if (this.state !== state) {
+      this.props.search(this.state.search, this.state.searchType, this.props.enemies)  
+    }
   }
 
   render() {
-    const options = Object.keys(SearchType).map(k => ({ text: k, value: (SearchType as any)[k] }));
+    const options = Object.keys(SearchType).map(k => ({ text: k, value: (SearchType as Record<string, string>)[k] }));
     return (
       <div className='search-bar panel-block'>
         <div className='field has-addons'>
           <p className='control'>
             <span className='select'>
-              <select value={this.state.searchType} onChange={this.handleSearchTypeChange}>
+              <select value={this.state.searchType} onChange={(value) => this.handleSearchTypeChange(value)}>
                 {options.map((option, i) => (<option key={i} value={option.value}>{option.text}</option>))}
               </select>
             </span>
           </p>
           <p className='control is-expanded has-icons-left'>
-            <input value={this.state.value} onChange={this.handleSearchChange}
+            <input value={this.state.search} onChange={(value) => this.handleSearchChange(value)}
               className='input' type='text' placeholder='Write your search' />
             <span className='icon is-left'>
               <i className='fas fa-search' aria-hidden='true'></i>
